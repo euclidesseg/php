91b5d2826acfe9eb49e54ccbb009a9ab54f83971
EUCLIDES PEREZ <89555967+euclidesseg@users.noreply.github.com>
2022-05-26 21:20:07 -0500

    Update inicio.php

--- FUNDAMENTOS PHP/inicio.php
+++ FUNDAMENTOS PHP/inicio.php
@@ -7,4 +7,18 @@
     
     echo $saludo." ".$nombre;
     print "<h1>hola yo soy </h1>";
+
+
+    $nombre = $_GET[nombre];
+    $apellido = $_GET[apellido];
+    $telefono = $_GET[tel];
+    
+    if(is_numeric($nombre)){
+        echo "elnombre no es correcto";
+    }else{
+        echo $nombre." ". $apellido. " ". $telefono;
+    }
+
+    
+
 ?>
