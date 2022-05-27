5f84de666d74ee2c5c5e5af1da3137a184e2be42
EUCLIDES PEREZ <89555967+euclidesseg@users.noreply.github.com>
2022-05-26 20:33:43 -0500

    Create index.php

---
+++ FUNDAMENTOS PHP/index.php
@@ -0,0 +1,36 @@
+<?php
+    echo "desde index <br>";
+    // declarar arreglos 
+    #1
+    $frutas = array("mango", "sandia","fresa");
+    #2
+    $verduras = array("tomate", "cebolla","sandia");
+
+    array_push($frutas,"mora","zapote","piña"); //esto me indica que voy a agrgar mas elementos al arreglo de futas y de ñame 
+    array_push($verduras,"papa","ñame","yuca");
+    
+
+    echo $frutas[2]. "". $verduras[4];//de esta menera imprimimos manual mente 
+
+    //forma de imprimir el arreglo recorriendolo
+// for($i = 0; $i < count($frutas); $i++){
+//     echo $i ."". $frutas[$i]."<br>";
+
+    #arreglos asociativos
+    $productos = [
+        "arroz" => "roa",
+        "acerte" => "primier",
+        "arepa" => "doña pepita",
+    ];
+    echo "<ol>";
+    #recorrer los arreglos asociativos 
+    foreach($productos as $marca ){
+        echo "marcas : ". $marca . "<br>";
+        foreach($marca as $mar ){
+            echo "<li>". $mar ."</li>";
+        }
+    }
+
+
+echo "</ol>";
+?>
